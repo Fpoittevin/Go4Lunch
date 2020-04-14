4d43d006652da8443af664c1531144fb6ca01145
Fpoittevin <dev@francoispoittevin.Fr>
2020-04-14 16:23:21 +0200

    creation of SignInActivity

---
+++ app/src/main/java/com/ocr/francois/go4lunch/ui/signin/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.ocr.francois.go4lunch.ui.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ocr.francois.go4lunch.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
